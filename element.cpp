79e41e3f282bd1da6750974b8081abacaa30ea85
khoanguyen12345 <67582698+khoanguyen12345@users.noreply.github.com>
2023-10-06 19:19:24 -0400

    finished element .h file

--- element.cpp
+++ element.cpp
@@ -1,39 +1,28 @@
+#include <element.h>
+
 template <class T>
-MinQueue<T>::MinQueue(void){ //constructor
-    heap_size = 0;
+Element<T>::Element(void){ //constructor
+    data = NULL;
+    key = NULL;
+    prev = NULL;
+    next = NULL; 
 }
 
 template <class T>
-MinQueue<T>::~MinQueue(void){ //destructor
-    heap.resize(0);
-    heap_size = 0;
+Element<T>::~Element(void){ //destructor
+    prev = NULL;
+    next = NULL; 
 }
 
 template <class T>
-MinQueue<T>::MinQueue(T* myMinQueue, int n){ //copy constructor
-/*
-    Pre-condition:
-    - n must match the size of myMinQueue
-    - elements in myMinqueue must match data type declared in original minqueue declaration
+Element<T>::Element(const Element<T> &copied_element){ //destructor
+    prev = copied_element.prev;
+    next = copied_element.next; 
+    key = copied_element.key;
+    data = copied_element.data;
+}
 
-    Post-condition:
-    - returns a vector that follows min-heap properties, implemented as a MinQueue object
-    - all elements in myMinQueue are in the returned heap.
-
-    Purpose: 
-    - copies elements from an array into a minqueue, following max heap proerties
-
-    Return: returns a vector that follows min-heap properties, implemented as a MinQueue object
-*/
-    if (n<1){ 
-        return;
-    }
-    heap.resize(n); 
-    heap_size = n; 
-    for (int i = 0; i < heap_size; i++)
-    {
-        heap[i] = myMinQueue[i];
-    }
-    build_heap();
-    return;
+template <class T>
+Element<T>::Element(T data,int key){ 
+    
 }
--- element.h
+++ element.h
@@ -9,12 +9,19 @@ class Element
     public:
         Element(void); //constructor
         ~Element(void); //destructor
-        Element(T* Element); //copy constructor from another element
+        Element(const Element<T> &copied_element); //copy constructor from another element
         Element(T data,int key); //copy constructor by inserting in data and a key value
         void get_key (); //returns numeric value stored inside element
         void get_data (); //returns string data stored inside element
 
+    
     private:
-        T data;
-        int key;  
-};
+    struct Node
+	{
+		T item;
+        Node *prev;
+		Node 	*next;
+        int key;
+        
+	};
+};  
