82d1f794332e01e17c8f4a2c0ba4277fb47091e5
Niranjan <73375333+niranjanreji@users.noreply.github.com>
2023-10-07 19:33:57 -0400

    Update element.h

--- element.h
+++ element.h
@@ -16,10 +16,10 @@ class Element
             
         };
     public:
-        Element(void); //constructor
-        ~Element(void); //destructor
+        Element(); //constructor
+        ~Element(); //destructor
         Element(const Element<T> &copied_element); //copy constructor from another element
         Element(T inputData,int inputKey); //copy constructor by inserting in data and a key value
-        void get_key () const; //returns numeric value stored inside element
-        void get_data () const; //returns string data stored inside element
+        void get_key() const; //returns numeric value stored inside element
+        void get_data() const; //returns string data stored inside element
 };  
