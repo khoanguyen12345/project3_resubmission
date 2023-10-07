aaa5ee7ba5f37e768d7a69ab4e35aac51978d1ae
Niranjan <73375333+niranjanreji@users.noreply.github.com>
2023-10-07 18:57:15 -0400

    added method definitions

--- hash_table.h
+++ hash_table.h
@@ -6,7 +6,13 @@ template <class T>
 class HashTable
 {
     private:
+        T* hashtable;
         int size;   
     public:
         HashTable(void); //constructor
+        HashTable(int size);
+        void insert(T data, int key);
+        void remove(int key);
+        void member(T data, int key);
+        string to_string();
 };
