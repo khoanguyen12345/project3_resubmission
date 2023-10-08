9c211b8b97d68e24cf45b53e20eb85831c613cd5
Niranjan <73375333+niranjanreji@users.noreply.github.com>
2023-10-08 16:47:42 -0400

    usecase skeleton

---
+++ usecase.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "hash_table.h"
+#include "element.h"
+
+using namespace std;
+
+template <class T>
+HashTable<T>* create_table(string fname, int m) {
+
+}
+
+template <class T>
+bool login(HashTable<T>* ht, T username, string password) {
+
+}
