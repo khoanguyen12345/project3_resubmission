e700ff9857f9348e0c049ec5267f9a7fe2a4e26d
Niranjan <73375333+niranjanreji@users.noreply.github.com>
2023-10-09 18:52:36 -0400

    small changes to usecase (DONT MAKE)

--- usecase.cpp
+++ usecase.cpp
@@ -7,16 +7,22 @@
 // ================================================
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
 #include "hash_table.h"
 #include "element.h"
 
 using namespace std;
-/*
+
 template <class T>
 HashTable<T>* create_table(string fname, int m) {
+    fstream f;
+    fname = fname + ".csv";            //is this really needed?
+    f.open(fname, ios::in);
 }
 
 template <class T>
 bool login(HashTable<T>* ht, T username, string password) {
 }
-*/
