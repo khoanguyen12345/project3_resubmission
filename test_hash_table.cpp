61260ade3a6978b17406800e07b79ba37d8d1f71
Niranjan <73375333+niranjanreji@users.noreply.github.com>
2023-10-11 15:54:19 -0400

    fixes

--- test_hash_table.cpp
+++ test_hash_table.cpp
@@ -605,8 +605,8 @@ void test_login() {
         username = "ee";
         password = "";
         if(login(ht, username, password)){cout << "Incorrect result validating blank password information" << endl;}
-        username = 123;
-        password = 333;
+        username = "123";
+        int password = 333;
         if(login(ht, username, password)){cout << "Incorrect result validating input type" << endl;}
     } catch(exception& e) {
         cerr << "Error in accomplishing login validation : " << e.what() << endl;
